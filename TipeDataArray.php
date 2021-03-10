57a412619c42020c7299ebea0518be8df212264a
Martin Lumban Gaol <martinlumbangaol880@gmail.com>
2021-03-10 11:51:06 +0700

    small changes

--- TipeDataArray.php
+++ TipeDataArray.php
@@ -1,40 +1,40 @@
 <?php
 
-$values = array(10, 9, 8, 7.5);
-var_dump($values);
+// $values = array(10, 9, 8, 7.5);
+// var_dump($values);
 
-$names = ["Martin", "Halomoan", "Lumbangaol"];
-var_dump($names);
+// $names = ["Martin", "Halomoan", "Lumbangaol"];
+// var_dump($names);
 
-var_dump($names[0]);
+// var_dump($names[0]);
 
-$names[0] = "Budi";
-var_dump($names);
+// $names[0] = "Budi";
+// var_dump($names);
 
-unset($names[1]);
-var_dump($names);
+// unset($names[1]);
+// var_dump($names);
 
-$names[] = "Joko";
-var_dump($names);
+// $names[] = "Joko";
+// var_dump($names);
 
-var_dump(count($names));
+// var_dump(count($names));
 
 // ====================================================================== //
 
 // array of map
-// $martin = array(
-//     "id" => "mrt",
-//     "name" => "Martin Lumbangaol",
-//     "age" => 29
-// );
-// var_dump($martin);
+$martin = array(
+    "id" => "mrt",
+    "name" => "Martin Lumbangaol",
+    "age" => 29
+);
+var_dump($martin);
 
-// var_dump($martin["name"]);
+var_dump($martin["name"]);
 
-// $iska = [
-//     "id" => "isk",
-//     "name" => "Iska Heriyati",
-//     "age" => 26
-// ];
-// var_dump($iska);
+$iska = [
+    "id" => "isk",
+    "name" => "Iska Heriyati",
+    "age" => 26
+];
+var_dump($iska);
 
