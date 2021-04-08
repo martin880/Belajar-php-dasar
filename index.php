41d4601a529ca4052fe096519e3fc0714de99f67
Martin Lumban Gaol <martinlumbangaol880@gmail.com>
2021-04-08 22:46:00 +0700

    tes

--- index.php
+++ index.php
@@ -1,3 +1,11 @@
 <?php
 
-echo "Hello-World";
+function sapaHello(string $first, string $middle, string $last): void
+{
+    echo "Hello $first $middle $last" . PHP_EOL;
+}
+
+sapaHello("Martin", "Halomoan", "Lumbangaol");
+
+// named Argument => fitur dari PHP versi 8
+sapaHello(last: "Lumbangaol", first: "Martin", middle: "Halomoan");
